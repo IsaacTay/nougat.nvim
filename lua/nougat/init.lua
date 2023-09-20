beb49aa072fd66c52bb7b12c5d4b0e934be06d0f
Isaac Tay <IsaacTay@pm.me>
2023-09-20 11:40:35 +0800

    chore: refactor some lines into utils functions

--- lua/nougat/init.lua
+++ lua/nougat/init.lua
@@ -1,4 +1,5 @@
 local config = require("nougat.config")
+local utils = require("nougat.utils")
 
 local M = {}
 
@@ -15,40 +16,23 @@ function M.enable()
     callback = function()
       local buf_id = vim.api.nvim_get_current_buf();
       local file_path = vim.api.nvim_buf_get_name(buf_id)
-      vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, { "PLACEHOLDER TEXT: Will be replaced once nougat completes", "Nougat log:" })
+      utils.set_lines(buf_id, 0, -1, { "PLACEHOLDER TEXT: Will be replaced once nougat completes", "Nougat log:" })
       local first_line = 0
-      vim.bo[buf_id].modifiable = false
-      vim.bo[buf_id].modified = false
       local shell_fn = { "nougat", "--markdown", file_path }
-      print(vim.inspect(shell_fn))
       vim.fn.jobstart(shell_fn, {
         on_stdout = function(_, data)
           if data then
             table.remove(data)
-            for i, line in ipairs(data) do
-              if line:sub(-1, -1) == "\r" then -- To remove carriage return for windows
-                data[i] = line:sub(1, -2)
-              end
-            end
-            vim.bo[buf_id].modifiable = true
-            vim.api.nvim_buf_set_lines(buf_id, first_line, -1, false, data)
+            utils.remove_cr(data)
+            utils.set_lines(buf_id, first_line, -1, data)
             first_line = -1
-            vim.bo[buf_id].modifiable = false
-            vim.bo[buf_id].modified = false
           end
         end,
         on_stderr = function(_, data)
           if data then
             table.remove(data)
-            for i, line in ipairs(data) do
-              if line:sub(-1, -1) == "\r" then -- To remove carriage return for windows
-                data[i] = line:sub(1, -2)
-              end
-            end
-            vim.bo[buf_id].modifiable = true
-            vim.api.nvim_buf_set_lines(buf_id, -1, -1, false, data)
-            vim.bo[buf_id].modifiable = false
-            vim.bo[buf_id].modified = false
+            utils.remove_cr(data)
+            utils.set_lines(buf_id, -1, -1, data)
           end
         end
       })
---
+++ lua/nougat/utils.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.remove_cr(lines)
+  for i, line in ipairs(lines) do
+    if line:sub(-1, -1) == "\r" then -- To remove carriage return for windows
+      lines[i] = line:sub(1, -2)
+    end
+  end
+  return lines
+end
+
+function M.set_lines(buf, start, stop, lines)
+  vim.bo[buf_id].modifiable = true
+  vim.api.nvim_buf_set_lines(buf, start, stop, false, lines)
+  vim.bo[buf_id].modifiable = false
+  vim.bo[buf_id].modified = false
+end
+
+return M
